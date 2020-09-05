1751c56d61b37cc6705018021243c3b420f0e06a
beata <beata@emc2wave.com>
2020-09-04 17:29:06 -0700

    add form do AddPlayer

--- src/AddPlayer.svelte
+++ src/AddPlayer.svelte
@@ -4,3 +4,9 @@
     points: 0,
   };
 </script>
+
+<form class="grid-3">
+  <input type="text" placeholder="Player Name" bind:value={player.name} />
+  <input type="number" placeholder="Player Points" bind:value={player.points} />
+  <input type="submit" class="btn btn-primary" value="Add Player" />
+</form>
